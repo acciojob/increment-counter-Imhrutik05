d9fccd4e006f21b9da0927bb9ae740893f7e4162
Hrutik Chaudhari <hrutikchaudhari05@gmail.com>
2025-08-14 21:18:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+const counter = document.getElementById("counter");
+const button = document.getElementById("incrementBtn");
+
+let counterValue = 0;
+
+button.addEventListener("click" , function() {
+	alert(counterValue);
+
+	counterValue++;
+
+	counter.textContent = counterValue;
+});
